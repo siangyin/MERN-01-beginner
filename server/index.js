08473cda3ed2283023e129c769f70cc97323ab0d
Lee Siang Yin <siangyin916@gmail.com>
2022-01-03 20:30:10 +0800

    ✅ basic get and post controller. 🆗

--- server/index.js
+++ server/index.js
@@ -3,6 +3,24 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3030;
 const connectDB = require("./db/connectDB");
+const UserModel = require("./models/Users");
+
+app.use(express.json());
+
+app.get("/getUsers", async (req, res) => {
+	const users = await UserModel.find({});
+	res.status(200).json(users);
+});
+
+app.post("/createUser", async (req, res) => {
+	try {
+		const user = req.body;
+		const newUser = await UserModel.create(user);
+		res.status(200).json(newUser);
+	} catch (error) {
+		console.log(error);
+	}
+});
 
 async function start() {
 	try {
